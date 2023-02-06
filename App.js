6f46ed18f4c74f1b08ccdaa45d7b88ac65866dfe
pekkao <pekka.ojala@oamk.fi>
2023-02-06 10:03:31 +0200

    Timer stop and conditional rendering

--- App.js
+++ App.js
@@ -5,24 +5,32 @@ export default function App() {
 
   const [words, setWords] = useState([null, "First", "Second", "Third"]);
   const [wordIndex, setWordIndex] = useState(1);
-  const [counter, setCounter] = useState(10);
+  const [counter, setCounter] = useState(3);
+  const [test, setTest] = useState(false);
 
   useEffect(() => {
     const timer = counter > 0 && setInterval(() => setCounter(counter - 1), 1000);
-    if (counter === 0 && wordIndex < words.length) {
-      setCounter(10);
+    if (counter === 0 && wordIndex < (words.length - 1)) {
+      setCounter(3);
       setWordIndex(wordIndex + 1);
     }
-    return () => clearInterval(timer);
+    if (counter === 0 && wordIndex === (words.length - 1)) {
+      setTest(true);
+    }
+    return () => {
+      clearInterval(timer);
+    }
   }, [counter]);
 
   return (
     <View style={styles.container}> 
-      {wordIndex < words.length && 
+      {!test ?
         <>
           <Text>Word {wordIndex}: {words[wordIndex]}</Text>
           <Text>Time left: {counter}</Text>
         </>
+      :
+        <Text>No time left</Text>
       }
     </View>
   );
